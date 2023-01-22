b555397f9033a96324ae2c440d34b4b5d948a74a
libian19 <libiansimpson19@gmail.com>
2023-01-21 22:50:56 -0400

    Estructura inicial de la calculadora

---
+++ src/componentes/Pantalla.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../styles/Pantalla.css'
+
+const Pantalla = ( {output} ) => (
+    <div className='output'>
+        {output}
+    </div>
+);
+
+export default Pantalla;
